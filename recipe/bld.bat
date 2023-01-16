3f088adb5ceea0a0a0e4e582bc28426aa12394b7
Raimondas Galvelis <r.galvelis@acellera.com>
2023-01-16 12:35:04 +0100

    Fix OpenCL linking

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,6 @@ cd build
 cmake %SRC_DIR% -G Ninja ^
                 -DCMAKE_BUILD_TYPE=Release ^
                 -DOPENMM_DIR=%LIBRARY_PREFIX% ^
-                -DOpenCL_INCLUDE_DIR=%LIBRARY_PREFIX%\lib\include ^
-                -DOPENCL_LIBRARIES=%LIBRARY_PREFIX%\lib\OpenCL.lib ^
                 -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
                 || exit /B 1
 
