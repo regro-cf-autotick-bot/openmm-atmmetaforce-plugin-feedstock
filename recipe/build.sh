7990d5353cc6a2a93fee77f75f023912b2cd5228
Raimondas Galvelis <r.galvelis@acellera.com>
2023-02-07 13:29:59 +0100

    Remove a leftover

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,7 @@ cmake --build .
 cmake --install .
 
 # Build and install Python components
-cmake --build . -- PythonInstall || exit /B 1
+cmake --build . -- PythonInstall
 
 for lib in ${PREFIX}/lib/plugins/*${SHLIB_EXT}; do
     ln -s $lib ${PREFIX}/lib/$(basename $lib) || true
